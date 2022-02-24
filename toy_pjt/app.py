1570a218b52b576fc0631cd8fc20d14d7cd59f06
DESKTOP-NFPDVT5\livia <ykw3480@gmail.com>
2022-02-24 21:11:53 +0900

    day03

--- toy_pjt/app.py
+++ toy_pjt/app.py
@@ -1,12 +1,14 @@
 from math import *
-
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
+from flask import *
+import hashlib
+import jwt
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
+import item_list
+
+app = Flask(__name__)
 client = MongoClient('mongodb+srv://test:sparta@cluster0.tfnms.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.toy_project
 
@@ -16,29 +18,18 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 items = soup.select('ul.calendar_list > li')
 
-for item in items:
-    image = item.select_one('div.img_area > img')['data-src'].strip()
-    brand = item.select_one('div.info_area > div.product_info > div.brand').text.strip()
-    product_name = item.select_one('div.info_area > div.product_info > div.name').text.strip()
-    draw_month = item.select_one('div.info_area > div.date_info > span.month').text.strip()
-    draw_date = item.select_one('div.info_area > div.date_info > span.date').text.strip()
-    doc = {
-        'image': image,
-        'brand': brand,
-        'product_name': product_name,
-        'draw_month': draw_month,
-        'draw_date': draw_date
-    }
+item_list.crawling()
 
-    if db.items.find_one({'product_name': product_name}) is None:
-        db.items.insert_one(doc)
-
-
+SECRET_KEY = 'SPARTA'
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    if request.cookies.get('token') is None:
+        return render_template('index.html')
+    else:
+        token = request.cookies.get('token')
+        member = jwt.decode(token, SECRET_KEY, algorithms = 'HS256')
+        return render_template('index.html', member=member)
 
 @app.route("/list", methods=["GET"])
 def item_list():
@@ -48,15 +39,25 @@ def item_list():
     last_page = ceil(items_count/12)
     return jsonify({'items': item_list, 'last_page': last_page})
 
-@app.route('/draw', methods=["GET"])
+@app.route('/draw', methods=["POST"])
 def draw():
     product_name = request.form['product_name']
-    db.users.update_one({'product_name':product_name},{'$push':{'draw_member':id}})
-    return render_template('index.html')
+    id = request.form['id']
+    item = db.items.find_one({'product_name': product_name})
 
-@app.route('/signup_form')
-def signup_form():
-    return render_template('login_resist_form.html')
+    if item.get('draw_member') is None:
+        db.items.update_one({'product_name': product_name}, {'$push': {'draw_member': id}})
+        db.member.update_one({'ID': id}, {'$push': {'draw_items': product_name}})
+        return jsonify({'msg': '응모가 완료되었습니다!'})
+
+    else:
+        if id in item['draw_member']:
+            return jsonify({'msg': '이미 응모 되어있는 상품입니다.'})
+
+        db.items.update_one({'product_name': product_name}, {'$push': {'draw_member': id}})
+        db.member.update_one({'ID': id}, {'$push': {'draw_items': product_name}})
+        return jsonify({'msg': '응모가 완료되었습니다!'})
+
 
 @app.route('/dupli_check', methods=["POST"])
 def duplication_check():
@@ -70,16 +71,43 @@ def duplication_check():
 def sign_up():
     id = request.form['id']
     password = request.form['pw']
+    pw_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
     name = request.form['name']
     doc={
         'ID':id,
-        'password':password,
+        'password':pw_hash,
         'name':name
     }
     db.member.insert_one(doc)
     return render_template('login_resist_form.html')
 
+@app.route("/sign_in", methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['pw']
 
+        pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+
+        result = db.member.find_one({'ID': id, 'password': pw_hash})
+
+        if result is not None:
+            payload = {
+                'ID': result['ID'],
+                'name': result['name'],
+                'draw_items': result.get('draw_items'),
+                'exp': datetime.utcnow() + timedelta(hours=24) # 로그인 24시간 유지
+            }
+
+            token = jwt.encode(payload, SECRET_KEY, algorithm = 'HS256')
+
+            resp = make_response(redirect(url_for('home')) )
+            resp.set_cookie('token', token)
+            return resp
+        else:
+            return render_template('login_resist_form.html', msg='아이디 또는 비밀번호가 일치하지 않습니다.')
+    else:
+        return render_template('login_resist_form.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
